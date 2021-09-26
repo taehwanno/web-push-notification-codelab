bada9718e6de9617ce0b9d9fde3c22c1d44b2311
Taehwan Noh <taehwanno.dev@gmail.com>
2021-09-26 23:50:54 +0900

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Global, css } from "@emotion/react";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css({
+          "html, body": {
+            padding: 0,
+            margin: 0,
+            fontFamily:
+              "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+          },
+          a: {
+            color: "inherit",
+            textDecoration: "none",
+          },
+          "*": {
+            boxSizing: "border-box",
+          },
+        })}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
